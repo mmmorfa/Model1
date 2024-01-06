7cef3b5b5116cc678410008eaba663794884333a
mmmorfa <mariomorfa97@gmail.com>
2024-01-06 11:24:44 +0100

    Load model

--- agent_slice_creation1.py
+++ agent_slice_creation1.py
@@ -8,13 +8,13 @@ from os.path import exists
 
 env = SliceCreationEnv1()
 
-'''
-if exists('/home/mario/Documents/DQN_Models/Model 1/gym-examples/dqn_slices1.zip'):
-    model = DQN.load("dqn_slices1")
-else: model = DQN("MlpPolicy", env, verbose=1, exploration_final_eps=0, exploration_fraction=0.5)
-'''
-#model = DQN.load("dqn_slices1")
-model = DQN("MlpPolicy", env, verbose=1, exploration_final_eps=0, exploration_fraction=0.5)
 
-model.learn(total_timesteps=10000, log_interval=1000)
+if exists('/home/mario/Documents/DQN_Models/Model 1/gym-examples/dqn_slices1.zip'):
+    model = DQN.load("dqn_slices1", env)
+else: model = DQN("MlpPolicy", env, verbose=1, exploration_final_eps=0, exploration_fraction=0.5)
+
+#model = DQN.load("dqn_slices1", env)
+#model = DQN("MlpPolicy", env, verbose=1, exploration_final_eps=0, exploration_fraction=0.5)
+
+model.learn(total_timesteps=5000, log_interval=1000)
 model.save("dqn_slices1")
